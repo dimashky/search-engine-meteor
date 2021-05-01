9d5046af5a490121ae5ecbc7116b3e0f38c927d1
Mohamed Dimashky <mohamed.khair.dimashky@gmail.com>
2021-05-01 18:54:41 +0300

    add extended boolean model

--- imports/api/methods/matcher.js
+++ imports/api/methods/matcher.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import BooleanModel from "../search-engine/models/BooleanModel";
 import VectorModel from "../search-engine/models/VectorModel";
+import ExtendedBooleanModel from "../search-engine/models/ExtendedBooleanModel";
 
 const TextProcessor = require("../search-engine/TextProcessor");
 
@@ -11,6 +12,10 @@ Meteor.methods({
         switch (selectedModel) {
             case "vector":
                 return VectorModel.handle(queryTokens);
+            case "ext-boolean":
+                return ExtendedBooleanModel.handle(queryTokens);
+            case "ext-or-boolean":
+                return ExtendedBooleanModel.handle(queryTokens, true);
             case "boolean":
             default:
                 return BooleanModel.handle(queryTokens);
---
+++ imports/api/search-engine/models/ExtendedBooleanModel.js
@@ -0,0 +1,31 @@
+import DocumentsFetcher from "../DocumentsFetcher";
+import TermWeightCalculator from "../TermWeightCalculator";
+import math from "mathjs";
+
+class ExtendedBooleanModel {
+    /**
+     * Get document related to query
+     *
+     * @param {string[]} query
+     * @param {boolean} orOperator
+     * @return {any[]} Documents
+     */
+    static handle(query, orOperator = false) {
+        const relatedTerms = DocumentsFetcher.getTermsDocuments(query);
+        const allDocuments = [...new Set(relatedTerms.reduce((prev, current) => prev.concat(current.documents.map(d => d.doc)), []))];
+        const model = allDocuments.map(document => {
+            return relatedTerms.map(term => term.documents.find(d => d.doc === document) ? TermWeightCalculator.TfIdf(term.term, document) : 0);
+        });
+        return allDocuments
+            .map((d, idx) => {
+                let relevance = orOperator
+                    ? math.pow(math.sum(model[idx].map(weight => math.pow(weight, 2))) / model[idx].length, 0.5)
+                    : 1 - math.pow(math.sum(model[idx].map(weight => math.pow(1 - weight, 2))) / model[idx].length, 0.5);
+                return {document: d, vector: model[idx], relevance }
+            })
+            .sort((d1, d2) => math.compare(d1.relevance, d2.relevance))
+            .reverse();
+    }
+}
+
+export default ExtendedBooleanModel;
